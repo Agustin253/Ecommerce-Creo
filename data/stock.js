c468da3e6a22ffa0f593d4b3a146f437ce6841c4
Agustin Creo <agustin.creo@gmail.com>
2023-03-04 08:14:00 -0300

    subimos el proyeto al repositorio

---
+++ data/stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Plato",
+        desc: "Plato de cerámica",
+        precio: 1200,
+        imagen: '../public/images/plato.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Animalito",
+        desc: "Maceta con forma de animalitos",
+        precio: 500,
+        imagen: '../public/images/animalito.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Desayuno",
+        desc: "Juego de desayuno en cerámica",
+        precio: 900,
+        imagen: '../public/images/Desayuno.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Mate",
+        desc: "Mate de cerámica artesanal",
+        precio: 1600,
+        imagen: '../public/images/mate.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Maceta",
+        desc: "Maceta de cerámica artesanal",
+        precio: 1200,
+        imagen: '../public/images/maceta.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Porta Vela",
+        desc: "Porta vela de cerámica artesanal",
+        precio: 500,
+        imagen: '../public/images/vela.jpg',
+        cantidad:1
+    }
+];
